496ea1eae303b8cae7b7783d2d5009b2a48b3a9b
SARVJEET SINGH <singhsarvjeet9876@gmail.com>
2026-06-02 17:09:15 +0530

    init commit

---
+++ src/NumberisPalindrome.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class NumberisPalindrome {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter The Number : ");
+        int num = sc.nextInt();
+        int original = num;
+        int reverse = 0;
+
+        while (num > 0) {
+            reverse = reverse * 10 + (num % 10);
+            num /= 10;
+        }
+
+        if (original == reverse)
+            System.out.println("The Number is Palindrome");
+        else
+            System.out.println("The Number is Not Palindrome");
+    }
+}

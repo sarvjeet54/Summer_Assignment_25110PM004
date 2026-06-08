649a09286c2d83dbc1b0c250012dc2e0d9870d28
Sarvjeet Singh <singhsarvjeet9876@gmail.com>
2026-06-08 21:53:29 +0530

    Add files via upload

---
+++ Day 7/RecursiveReverse.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class RecursiveReverse {
+
+    static int rev = 0;
+
+    static int reverse(int n) {
+        if (n == 0)
+            return rev;
+
+        rev = rev * 10 + (n % 10);
+        return reverse(n / 10);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        System.out.println("Reverse = " + reverse(num));
+    }
+}

ee351fee7f91aac86465db22ba4ad74e42daaf8a
Sarvjeet Singh <singhsarvjeet9876@gmail.com>
2026-06-10 21:54:16 +0530

    Add files via upload

---
+++ Day 9/HollowSquare.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class HollowSquare {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Row : ");
+        int m = sc.nextInt();
+        System.out.print("Enter Column : ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= m ; i++) {
+            for (int j = 1; j <= n ; j++) {
+                if(i==1 || i==m || j==1 || j==n)
+                    System.out.print("* ");
+                else System.out.print("  ");
+
+            }
+            System.out.println();
+
+        }
+
+    }
+}
+
